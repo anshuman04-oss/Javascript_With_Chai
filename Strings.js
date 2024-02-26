2345c5413e07e9abfade6d782b830a1f4d40afb0
anshuman04-oss <141833342+anshuman04-oss@users.noreply.github.com>
2024-02-26 12:41:14 +0000

    update

---
+++ Strings.js
@@ -0,0 +1,21 @@
+// Outdated style of using strings
+
+const name = "Anshuman"
+const rollNo = "23CH10006"
+
+//     console.log(name + " and the roll no is " + rollNo);
+
+// The upper technique is an outdated style of concatenating strings
+
+// The modern method includes
+
+    // console.log(`My name is ${name}, and my roll no is ${rollNo}`);
+
+// ONE MORE METHOD FOR DECLARING STRINGS
+let userName = new String("anshu..");
+    console.log(`${userName}`);
+
+// In strings, the objects are accessed according to array indexing, but they should not be confused with array.
+// Strings are the objects. The index and the element are the key-value pairs.
+
+
